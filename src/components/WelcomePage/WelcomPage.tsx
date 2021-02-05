5b87c1a04732a63bfa6bf2a148b472324e8579df
apekhnik <iam3ji@gmail.com>
2021-02-05 17:30:46 +0300

    1.2

--- src/components/WelcomePage/WelcomPage.tsx
+++ src/components/WelcomePage/WelcomPage.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Particles from 'react-particles-js';
 import { useSelector } from 'react-redux';
-import { useSpring, animated } from "react-spring";
+import { useSpring, animated, config } from "react-spring";
 import { AppstateType } from '../../redux/store';
 import './welcome.sass'
 const WelcomePage = () => {
@@ -9,6 +9,7 @@ const WelcomePage = () => {
 
     const { x } = useSpring({
         x: isOpen ? 0 : 100,
+        config: config.molasses
     });
     return (
         <div className='check'
@@ -16,14 +17,9 @@ const WelcomePage = () => {
             <animated.div
                 style={{
                     transform: x.interpolate((x: any) => `translate3d(${x * -1}%,0,0)`),
+
                 }}
                 className='checkout_left'
-            >
-
-            </animated.div>
-            <animated.div
-                style={{ transform: x.interpolate((x) => `translate3d(${x}%,0,0)`) }}
-                className='checkout_right'
             >
                 <Particles
                     params={{
@@ -54,10 +50,10 @@ const WelcomePage = () => {
                                 },
                             },
                             opacity: {
-                                value: 0.5,
-                                random: false,
+                                value: 0.8,
+                                random: true,
                                 anim: {
-                                    enable: false,
+                                    enable: true,
                                     speed: 1,
                                     opacity_min: 0.1,
                                     sync: false,
@@ -86,7 +82,7 @@ const WelcomePage = () => {
                                 direction: "none",
                                 random: true,
                                 straight: true,
-                                out_mode: "destroy",
+                                out_mode: "bounce",
                                 bounce: true,
                                 attract: {
                                     enable: false,
@@ -138,7 +134,124 @@ const WelcomePage = () => {
                     }}
                 />
             </animated.div>
-        </div>
+            <animated.div
+                style={{ transform: x.interpolate((x) => `translate3d(${x}%,0,0)`) }}
+                className='checkout_right'
+            >
+                <Particles
+                    params={{
+                        particles: {
+                            number: {
+                                value: 130,
+                                density: {
+                                    enable: true,
+                                    value_area: 800,
+                                },
+                            },
+                            color: {
+                                value: "#ffffff",
+                            },
+                            shape: {
+                                type: "circle",
+                                stroke: {
+                                    width: 0,
+                                    color: "#000000",
+                                },
+                                polygon: {
+                                    nb_sides: 5,
+                                },
+                                image: {
+                                    src: "img/github.svg",
+                                    width: 100,
+                                    height: 100,
+                                },
+                            },
+                            opacity: {
+                                value: 0.8,
+                                random: true,
+                                anim: {
+                                    enable: true,
+                                    speed: 1,
+                                    opacity_min: 0.1,
+                                    sync: false,
+                                },
+                            },
+                            size: {
+                                value: 10,
+                                random: true,
+                                anim: {
+                                    enable: true,
+                                    speed: 8,
+                                    size_min: 5,
+                                    sync: false,
+                                },
+                            },
+                            line_linked: {
+                                enable: false,
+                                distance: 500,
+                                color: "#ffffff",
+                                opacity: 0.4,
+                                width: 2,
+                            },
+                            move: {
+                                enable: true,
+                                speed: 3,
+                                direction: "none",
+                                random: true,
+                                straight: true,
+                                out_mode: "bounce",
+                                bounce: true,
+                                attract: {
+                                    enable: false,
+                                    rotateX: 600,
+                                    rotateY: 1200,
+                                },
+                            },
+                        },
+                        interactivity: {
+                            detect_on: "canvas",
+                            events: {
+                                onhover: {
+                                    enable: false,
+                                    mode: "repulse",
+                                },
+                                onclick: {
+                                    enable: true,
+                                    mode: "push",
+                                },
+                                resize: true,
+                            },
+                            modes: {
+                                grab: {
+                                    distance: 800,
+                                    line_linked: {
+                                        opacity: 1,
+                                    },
+                                },
+                                bubble: {
+                                    distance: 800,
+                                    size: 80,
+                                    duration: 2,
+                                    opacity: 0.8,
+
+                                },
+                                repulse: {
+                                    distance: 110,
+                                    duration: 0.4,
+                                },
+                                push: {
+                                    particles_nb: 4,
+                                },
+                                remove: {
+                                    particles_nb: 2,
+                                },
+                            },
+                        },
+                        retina_detect: true,
+                    }}
+                />
+            </animated.div>
+        </div >
     )
 }
 export default WelcomePage
